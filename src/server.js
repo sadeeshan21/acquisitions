b55b35fc1377285a8d35d6300c8e68c1b9b0687d
sadeepa <sadeepasamarasinghe210215@gmail.com>
2026-06-20 03:27:52 +0530

    add winston,helemt,cors

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ import app from './app.js';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Listening on  http://lo${PORT}`);
+  console.log(`Listening on  http://localhost:${PORT}`);
 });
